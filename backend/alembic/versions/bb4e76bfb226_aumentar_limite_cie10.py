9180a821c4f2748f858710f13524ef7ee3deef4e
Francisco Flores <is728238@iteso.mx>
2026-07-01 00:36:38 -0600

    style: organizar importaciones en la migracion de cie10 (ruff)

--- backend/alembic/versions/bb4e76bfb226_aumentar_limite_cie10.py
+++ backend/alembic/versions/bb4e76bfb226_aumentar_limite_cie10.py
@@ -7,9 +7,9 @@ Create Date: 2026-07-01 00:32:32.011748
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'bb4e76bfb226'
